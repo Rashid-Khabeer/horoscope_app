cf89a08b3015d69f13e1fa58f5ac27f3ecb4d01b
Rashid-khabeer <rkhabeer84@gmail.com>
2021-04-06 00:14:34 +0500

    added local data variables

--- lib/src/data/data.dart
+++ lib/src/data/data.dart
@@ -1,4 +1,4 @@
- import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppData {
   static SharedPreferences _preferences;
@@ -9,6 +9,9 @@ class AppData {
   static final _isNew = 'IsNew';
   static final _starName = 'StarName';
   static final _dateOfBirth = 'DateOfBirth';
+  static final _email = 'Email';
+  static final _isSignIn = 'IsSignIn';
+  static final _hasPayed = 'HasPayed';
 
   setIsNew(bool isNew) async => await _preferences.setBool(_isNew, isNew);
 
@@ -19,10 +22,22 @@ class AppData {
 
   String getStarName() => _preferences.getString(_starName) ?? '';
 
+  setEmail(String email) async => await _preferences.setString(_email, email);
+
+  String getEmail() => _preferences.getString(_email) ?? '';
+
   setDateOfBirth(String dateOfBirth) async =>
       await _preferences.setString(_dateOfBirth, dateOfBirth);
 
   String getDateOfBirth() => _preferences.getString(_dateOfBirth) ?? '';
+
+  setIsSignIn(bool value) async => await _preferences.setBool(_isSignIn, value);
+
+  bool getSignIn() => _preferences.getBool(_isSignIn) ?? false;
+
+  setHasPayed(bool value) async => await _preferences.setBool(_hasPayed, value);
+
+  bool getHasPayed() => _preferences.getBool(_hasPayed) ?? false;
 
   Future clearData() async => await _preferences.clear();
 }
